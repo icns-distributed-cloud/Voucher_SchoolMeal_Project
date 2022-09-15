45e1ba46861e8e4fa1d1d055c0d4becb69bf12ba
dlwmznzl1 <dlwmznzl1@naver.com>
2022-09-15 16:17:16 +0900

    Add YoloContainer Seol DetectObjectProc Code
    Add YoloContainer Seol DetectObjectProc Code

--- SchoolMeal_Part/DeepLearning/YoloContainer.py
+++ SchoolMeal_Part/DeepLearning/YoloContainer.py
@@ -1,9 +1,10 @@
 from FireDetect.DetectFiretProc import *
-
+# from ObjectDetect.ObjectFiretProc import * # 설
 class YoloContainer:
     
-    __mDetectFireProc = DetectFireProc
-    
+    __mDetectFireProc = DetectFireProc()
+    #__mDetectObjectProc = DetectObjectProc() # 설
     def Run(self):
 
         self.__mDetectFireProc.Run()
+        #self.__mDetectObjectProc.Run() # 설
