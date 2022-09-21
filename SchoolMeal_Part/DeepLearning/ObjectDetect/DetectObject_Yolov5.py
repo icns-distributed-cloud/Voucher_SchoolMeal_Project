15709d1c9ce32c299ab3be317d5d849e10f8c2a7
rohseol <seven800@naver.com>
2022-09-21 15:30:31 +0900

    edit to no detected file

--- SchoolMeal_Part/DeepLearning/ObjectDetect/DetectObject_Yolov5.py
+++ SchoolMeal_Part/DeepLearning/ObjectDetect/DetectObject_Yolov5.py
@@ -48,6 +48,34 @@ from utils.torch_utils import select_device, smart_inference_mode
 
 
 @smart_inference_mode()
+# def run(
+#         weights=ROOT / 'best.pt',  # model.pt path(s)
+#         source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
+#         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
+#         imgsz=(1440, 1080),  # inference size (height, width)
+#         conf_thres=0.25,  # confidence threshold
+#         iou_thres=0.45,  # NMS IOU threshold
+#         max_det=1000,  # maximum detections per image
+#         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+#         view_img=False,  # show results
+#         save_txt=False,  # save results to *.txt
+#         save_conf=False,  # save confidences in --save-txt labels
+#         save_crop=False,  # save cropped prediction boxes
+#         nosave=False,  # do not save images/videos
+#         classes=None,  # filter by class: --class 0, or --class 0 2 3
+#         agnostic_nms=False,  # class-agnostic NMS
+#         augment=False,  # augmented inference
+#         visualize=False,  # visualize features
+#         update=False,  # update all models
+#         project=ROOT / 'runs/detect',  # save results to project/name
+#         name='exp',  # save results to project/name
+#         exist_ok=False,  # existing project/name ok, do not increment
+#         line_thickness=3,  # bounding box thickness (pixels)
+#         hide_labels=False,  # hide labels
+#         hide_conf=False,  # hide confidences
+#         half=False,  # use FP16 half-precision inference
+#         dnn=False,  # use OpenCV DNN for ONNX inference
+# ):
 def run(
         weights=ROOT / 'best.pt',  # model.pt path(s)
         source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
@@ -67,8 +95,6 @@ def run(
         augment=False,  # augmented inference
         visualize=False,  # visualize features
         update=False,  # update all models
-        project=ROOT / 'runs/detect',  # save results to project/name
-        name='exp',  # save results to project/name
         exist_ok=False,  # existing project/name ok, do not increment
         line_thickness=3,  # bounding box thickness (pixels)
         hide_labels=False,  # hide labels
@@ -76,7 +102,6 @@ def run(
         half=False,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
 ):
-
     PixelList = []
 
     source = str(source)
