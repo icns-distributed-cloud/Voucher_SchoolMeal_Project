fd2b83e74c66cfe0654a86ca985b33dbc7d748a0
HongJu Jeong <sub06038@naver.com>
2022-10-17 14:39:39 +0900

    fixed led controller

--- controller/led_controller_v1.py
+++ controller/led_controller_v1.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 import threading
 import json
+#from SchoolMeal_Part.TIC_Data import *
 
-
-# GPIO.setmode(GPIO.BCM)  # 상위 컨트롤러에서 호출
+GPIO.setmode(GPIO.BCM)  # 상위 컨트롤러에서 호출
 
 class led_controller_v1(threading.Thread):
 
@@ -20,66 +20,70 @@ class led_controller_v1(threading.Thread):
 		self.red_channel = 27
 		self.blue_channel = 22
 		self.buzzer_channel = 10
-		
 		GPIO.setup(self.green_channel, GPIO.OUT)
 		GPIO.setup(self.blue_channel, GPIO.OUT)
 		GPIO.setup(self.buzzer_channel, GPIO.OUT)
 		GPIO.setup(self.red_channel, GPIO.OUT)
 
 	def light_on(self, pin):
-	GPIO.output(pin, GPIO.LOW)
+		GPIO.output(pin, GPIO.LOW)
 
 	def light_off(self, pin):
-	GPIO.output(pin, GPIO.HIGH)
+		GPIO.output(pin, GPIO.HIGH)
 
 	def run(self):
 		try:
-			light_off(green_channel)
-			light_off(red_channel)
-			light_off(blue_channel)
-			light_off(buzzer_channel)
+			self.gpio_setup()
+			time.sleep(0.5)
 
-				while(1):
-					time.sleep(1)
-					file = open('LightType.json')
-					jsonString = json.load(file)
-					lightType = jsonString.get("lightType")
-					file.close()
+			self.light_off(self.green_channel)
+			self.light_off(self.red_channel)
+			self.light_off(self.blue_channel)
+			self.light_off(self.buzzer_channel)
 
-					#lightType만 받고 다시 파일 값은 0으로 바꿔준다.
-					jsonString["lightType"] = 0
-					with open('LightType.json', 'w', encoding='utf-8') as f:
-						json.dump(jsonString, f, indent="\t")
-					f.close()
+			while(1):
+				time.sleep(1)
+				file = open('../SchoolMeal_Part/TIC_Data/LightType.json')
+				jsonString = json.load(file)
+				lightType = jsonString.get("lightType")
+				file.close()
 
-					if(lightType == 1):
-						light_on(red_channel)
-						time.sleep(2)
-						light_off(red_channel)
-					elif(lightType == 2):
-						light_on(red_channel)
-						light_on(blue_channel)
-						time.sleep(2)
-						light_off(red_channel)
-						light_off(blue_channel)
-					elif(lightType == 3):
-						light_on(red_channel)
-						light_on(green_channel)
-						time.sleep(2)
-						light_off(red_channel)
-						light_off(green_channel)
-					elif(lightType == 4):
-						light_on(buzzer_channel)
-						time.sleep(2)
-						light_off(buzzer_channel)
-					else:
-						GPIO.cleanup()
-						break
+				#lightType만 받고 다시 파일 값은 0으로 바꿔준다.
+				jsonString["lightType"] = 0
+				with open('../SchoolMeal_Part/TIC_Data/LightType.json', 'w', encoding='utf-8') as f:
+					json.dump(jsonString, f, indent="\t")
+				f.close()
+
+				if(lightType == 0):
+					continue
+				elif(lightType == 1):
+					self.light_on(self.red_channel)
+					time.sleep(2)
+					self.light_off(self.red_channel)
+				elif(lightType == 2):
+					self.light_on(self.red_channel)
+					self.light_on(self.blue_channel)
+					time.sleep(2)
+					self.light_off(self.red_channel)
+					self.light_off(self.blue_channel)
+				elif(lightType == 3):
+					self.light_on(self.red_channel)
+					self.light_on(self.green_channel)
+					time.sleep(2)
+					self.light_off(self.red_channel)
+					self.light_off(self.green_channel)
+				elif(lightType == 4):
+					self.light_on(self.buzzer_channel)
+					time.sleep(2)
+					self.light_off(self.buzzer_channel)
+				else:
+					GPIO.cleanup()
+					break
 
 					
-			except KeyboardInterrupt:
-				GPIO.cleanup()
-				pass
+		except KeyboardInterrupt:
+			GPIO.cleanup()
+			pass
 			
 con = led_controller_v1()
 con.run()
